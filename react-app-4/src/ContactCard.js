4ed1fffeaf9b14fd1d2cc5c3e1f3a170da03fa00
bug-dor <tolyaumer@rambler.ru>
2020-03-29 16:27:10 +0300

    modified: app,index and added: contactCard

---
+++ react-app-4/src/ContactCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ContactCard(props) {
+    console.log(props)
+    return (
+        <div className="contact-card">
+                <img align="center" src={props.contact.imgUrl}/>
+                <h3><font color="#3AC1EF">▍Mr. Whiskerson</font></h3>
+                <p>Phone: {props.contact.phone}</p>
+                <p>Email: {props.contact.email}</p>
+        </div>
+    )
+}
+export default ContactCard 
